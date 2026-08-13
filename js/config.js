9516ef03a7b19435505c4a8a15f03442562eddcd
omgidontknow <jacksmall@gmail.com>
2026-08-13 17:43:55 +0100

    Add configuration constants for game settings

--- js/config.js
+++ js/config.js
@@ -1,1 +1,47 @@
-m
+export const VERSION = "v0.7.0";
+
+
+/* WORLD */
+
+export const WORLD_WIDTH = 150;
+export const WORLD_HEIGHT = 190;
+
+export const CELL = 12;
+
+
+/* MATERIALS */
+
+export const AIR = 0;
+export const DIRT = 1;
+export const STONE = 2;
+export const BEDROCK = 3;
+
+
+/* MINING */
+
+export const MINING_RADIUS = 2.7;
+
+
+/* SAND */
+
+export const MAX_SAND = 10000;
+
+export const SAND_GRAIN_SIZE = 0.72;
+
+export const SAND_GRAVITY = 0.28;
+
+export const SAND_MAX_FALL_SPEED = 5;
+
+
+/* INVENTORY */
+
+export const INVENTORY_CAPACITY = 500;
+
+export const COLLECT_RADIUS = 30;
+
+
+/* CAMERA */
+
+export const MIN_ZOOM = 0.7;
+export const MAX_ZOOM = 3;
+export const DEFAULT_ZOOM = 1;
